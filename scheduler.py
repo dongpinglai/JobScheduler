2b02459dff3384979dcde10e58bd59b7f7145ded
laidongping <laidongping2006@sina.com>
2018-03-17 19:04:22 +0800

    临时提交

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@
 from apscheduler.schedulers.tornado import TornadoScheduler
 from apscheduler.executors.tornado import TornadoExecutor
 from apscheduler.jobstores.mongodb import MongoDBJobStore
+from apscheduler.util import undefined
 
 mongo_host = "localhost"
 mongo_port = 27017
@@ -25,12 +26,33 @@ scheduler = TornadoScheduler(jobstores=jobstores, executors=executors, job_defau
 class JobScheduler(object):
     def __init__(self):
         self._scheduler = scheduler
+        # 记录可以用任务函数
+        self._job_funcs= {}
 
-    def add_job(self, func, *args, **kwargs):
+    def register_func(self, func_name, func):
+        self._job_funcs[func_name] = func
+        
+    def remove_func(self, func_name):
+        self._job_funcs.pop(func_name, None)
+
+    def update_func(self, func_name, func):
+        self._job_funcs[func_name] = func
+
+    def find_func(self, func_name):
+        func = self._job_funcs.get(func_name)
+        return func
+
+    def add_job(self, func_name, trigger=None, args=None, kwargs=None, id=None, name=None, misfire_grace_time=undefined, coalesce=undefined, max_instances=undefined, next_run_time=undefined, jobstore="default", executor="default", replace_existing=False, **trigger_args):
         """
         新增任务
+        1.每个任务都预先设置一个id
+        2.如果id已经有一个任务，那就执行更新任务操作,否则，新增一个任务
         """
-        self._scheduler.add_job(func, *args, **kwargs)
+        func = self.find_func(func_name)
+        if func and callable(func):
+            self._scheduler.add_job(func, trigger, args, kwargs, id, name, misfire_grace_time, coalesce, max_instances, next_run_time, jobstore, executor, replace_existing, **trigger_args)
+        else:
+            raise TypeError("%s不是可调用对象" % func)
 
     def modify_job(self, job_id, jobstore=None, **changes):
         """改变任务属性"""
